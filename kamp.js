262ff665cc3d12d0f157d07abadfafc73fb0e4e7
tuanh1983 <122734267+tuanh1983@users.noreply.github.com>
2024-03-26 15:43:09 +0700

    new 02

--- kamp.js
+++ kamp.js
@@ -13,8 +13,13 @@ const vej_stykke_paavirket = "vej_stykke_paavirket.fgb";
 const bygning_paavirket = "bygning_paavirket.fgb";
 const timeout = '30m';
 const thinktime = '15s';
-const maxUser = 1;
-const duration = '1ms';
+const maxUser = 400;
+const duration_rampUp = '10m',duration_rampDown = '10m',duration = '10m';
+//const maxUser = 1;
+//const duration_rampUp = '1ms',duration_rampDown = '1ms',duration = '1ms';
+
+// This param should be update base on relase version
+
 const commonHeaders = {
   //"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36",
   "Cache-Control": "no-cache, no-store",
@@ -30,25 +35,20 @@ const commonHeaders = {
   Traceparent: "00-f64b6d00c81a4df589d004826523f7bb-a6621cce5d994a56-01",
 };
 
-// Those params for Debug ONLY
-const startTime = new Date();
+
 
 export const options = {
   stages: [
     // Warm-up stage: ramp up to 100 virtual users over 1 minute
-    { duration: duration, target: maxUser },
+    { duration: duration_rampUp, target: maxUser },
     
     // Sustained load stage: maintain 100 virtual users for 5 minutes
     { duration: duration, target: maxUser },
     
     // Ramp-down stage: gradually decrease the number of virtual users to 0 over 1 minute
-    { duration: duration, target: 0 },
+    { duration: duration_rampDown, target: 0 },
   ],
 };
-
-
-
-
 
 export default function () {
   var requests = [];
@@ -85,7 +85,9 @@ export default function () {
       });
     }
   });
-
+  // Those params for Debug ONLY
+  //const startTime = new Date(); 
+  // Those params for Debug ONLY
   const responses = http.batch(requests);
 
   responses.forEach((res, index) => {
@@ -104,9 +106,9 @@ export default function () {
     
    });
   // Those params for Debug ONLY
-  const endTime = new Date();
-  const duration = endTime - startTime;
-  console.log(`Transaction took ${duration} ms`);
+  // const endTime = new Date();
+  // const duration = endTime - startTime;
+  // console.log(`Transaction took ${duration} ms`);
   // Those params for Debug ONLY
 
 
